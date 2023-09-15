2db48832eba3f370eb5314920b137af03a7807af
romulo.banares <romulo.banares.contractor@bbva.com>
2023-09-15 12:41:17 +0200

    add next and previuos suggestion copilot key maps

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,4 @@
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-Z>", 'copilot#Next()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-X>", 'copilot#Previous()', { silent = true, expr = true })
